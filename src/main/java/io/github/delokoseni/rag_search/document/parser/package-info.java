d2a34c2b8a0898d69a71a19aa2fdc2b231b7908b
delokoseni <artur.b3lsh@gmail.com>
2026-06-25 12:28:57 +0700

    Добавлены файлы с комментариями для автоматического документирования.

---
+++ src/main/java/io/github/delokoseni/rag_search/document/parser/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Пакет парсеров документов.
+ *
+ * Отвечает за извлечение текста из различных форматов:
+ * - PDF (PDFBox)
+ * - DOCX (Apache POI)
+ *
+ * Преобразует файлы в единый текстовый формат.
+ */
+package io.github.delokoseni.rag_search.document.parser;
---
+++ src/main/java/io/github/delokoseni/rag_search/llm/openai/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Интеграция с OpenAI API.
+ *
+ * Отвечает за:
+ * - отправку запросов к моделям OpenAI
+ * - получение ответов и эмбеддингов
+ * - обработку API-ответов
+ */
+package io.github.delokoseni.rag_search.llm.openai;
