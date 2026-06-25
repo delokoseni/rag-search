8afbf1dd70fc45da7c7d0cac7dbe85405353e0ad
delokoseni <artur.b3lsh@gmail.com>
2026-06-25 11:06:49 +0700

    Initial commit.

---
+++ src/main/java/io/github/delokoseni/rag_search/RagSearchApplication.java
@@ -0,0 +1,13 @@
+package io.github.delokoseni.rag_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/delokoseni/rag_search/RagSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.delokoseni.rag_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
